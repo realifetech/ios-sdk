b64a30b4ac131c60cb15cfe326719ce8487e317f
J Albert <jonathon.albert@livestyled.com>
2020-10-22 11:38:47 +0100

    create test file

---
+++ SupportingModules/Storage/Tests/DiskStorageTests.swift
@@ -0,0 +1,23 @@
+//
+//  DiskStorageTests.swift
+//  RealifeTechTests
+//
+//  Created by Jonathon Albert on 22/10/2020.
+//  Copyright © 2020 Realife Tech. All rights reserved.
+//
+
+import XCTest
+@testable import RealifeTech
+
+class DiskStorageTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    
+}
