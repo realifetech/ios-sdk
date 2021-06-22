77de8e5029353bcc5dc4ab5037a8bf3e79385b3b
Mickey Lee <mickey.lee@realifetech.com>
2021-06-22 13:19:22 +0100

    ID-1028 Add fulfilment point filter

---
+++ Sell/FulfilmentPoint/Models/FulfilmentPointFilter.swift
@@ -0,0 +1,13 @@
+//
+//  FulfilmentPointFilter.swift
+//  RealifeTech
+//
+//  Created by Mickey Lee on 22/06/2021.
+//  Copyright © 2021 Realife Tech. All rights reserved.
+//
+
+import Foundation
+
+public struct FulfilmentPointFilter {
+    public let categories: [String]
+}
