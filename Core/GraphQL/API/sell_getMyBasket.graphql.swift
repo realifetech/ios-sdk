0a2ca2c42fb2c4c145b921f20458bdf87972aab1
Mickey Lee <mickey.lee@realifetech.com>
2021-06-23 17:26:02 +0100

    ID-1026 Update basket fragments, queries and mutations

---
+++ Core/GraphQL/API/sell_getMyBasket.graphql.swift
@@ -0,0 +1,122 @@
+// @generated
+//  This file was automatically generated and should not be edited.
+
+import Apollo
+import Foundation
+
+/// ApolloType namespace
+public extension ApolloType {
+  final class GetMyBasketQuery: GraphQLQuery {
+    /// The raw GraphQL definition of this operation.
+    public let operationDefinition: String =
+      """
+      query getMyBasket {
+        getMyBasket {
+          __typename
+          ...FragmentBasket
+        }
+      }
+      """
+
+    public let operationName: String = "getMyBasket"
+
+    public var queryDocument: String {
+      var document: String = operationDefinition
+      document.append("\n" + FragmentBasket.fragmentDefinition)
+      document.append("\n" + FragmentTimeslot.fragmentDefinition)
+      document.append("\n" + FragmentFulfilmentPoint.fragmentDefinition)
+      document.append("\n" + FragmentForm.fragmentDefinition)
+      document.append("\n" + FragmentFulfilmentPointCategory.fragmentDefinition)
+      document.append("\n" + FragmentVenue.fragmentDefinition)
+      document.append("\n" + FragmentProductVariant.fragmentDefinition)
+      document.append("\n" + FragmentProduct.fragmentDefinition)
+      document.append("\n" + FragmentProductModifierItem.fragmentDefinition)
+      document.append("\n" + FragmentProductModifierItemSelection.fragmentDefinition)
+      return document
+    }
+
+    public init() {
+    }
+
+    public struct Data: GraphQLSelectionSet {
+      public static let possibleTypes: [String] = ["Query"]
+
+      public static var selections: [GraphQLSelection] {
+        return [
+          GraphQLField("getMyBasket", type: .object(GetMyBasket.selections)),
+        ]
+      }
+
+      public private(set) var resultMap: ResultMap
+
+      public init(unsafeResultMap: ResultMap) {
+        self.resultMap = unsafeResultMap
+      }
+
+      public init(getMyBasket: GetMyBasket? = nil) {
+        self.init(unsafeResultMap: ["__typename": "Query", "getMyBasket": getMyBasket.flatMap { (value: GetMyBasket) -> ResultMap in value.resultMap }])
+      }
+
+      public var getMyBasket: GetMyBasket? {
+        get {
+          return (resultMap["getMyBasket"] as? ResultMap).flatMap { GetMyBasket(unsafeResultMap: $0) }
+        }
+        set {
+          resultMap.updateValue(newValue?.resultMap, forKey: "getMyBasket")
+        }
+      }
+
+      public struct GetMyBasket: GraphQLSelectionSet {
+        public static let possibleTypes: [String] = ["Basket"]
+
+        public static var selections: [GraphQLSelection] {
+          return [
+            GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+            GraphQLFragmentSpread(FragmentBasket.self),
+          ]
+        }
+
+        public private(set) var resultMap: ResultMap
+
+        public init(unsafeResultMap: ResultMap) {
+          self.resultMap = unsafeResultMap
+        }
+
+        public var __typename: String {
+          get {
+            return resultMap["__typename"]! as! String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "__typename")
+          }
+        }
+
+        public var fragments: Fragments {
+          get {
+            return Fragments(unsafeResultMap: resultMap)
+          }
+          set {
+            resultMap += newValue.resultMap
+          }
+        }
+
+        public struct Fragments {
+          public private(set) var resultMap: ResultMap
+
+          public init(unsafeResultMap: ResultMap) {
+            self.resultMap = unsafeResultMap
+          }
+
+          public var fragmentBasket: FragmentBasket {
+            get {
+              return FragmentBasket(unsafeResultMap: resultMap)
+            }
+            set {
+              resultMap += newValue.resultMap
+            }
+          }
+        }
+      }
+    }
+  }
+}
