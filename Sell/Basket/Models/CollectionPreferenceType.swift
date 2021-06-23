0e9a3be3e1b99d70f4fc61e2ff986fb0371102ca
Mickey Lee <mickey.lee@realifetech.com>
2021-06-23 17:44:21 +0100

    ID-1026 Add basket models

---
+++ Sell/Basket/Models/CollectionPreferenceType.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionPreferenceType.swift
+//  RealifeTech
+//
+//  Created by Mickey Lee on 23/06/2021.
+//  Copyright © 2021 Realife Tech. All rights reserved.
+//
+
+import Foundation
+
+public enum CollectionPreferenceType: String, Codable {
+    case asap = "ASAP"
+    case preorder = "PREORDER"
+    case checkin = "CHECKIN"
+}
---
+++ Sell/Basket/Models/SeatInfo.swift
@@ -0,0 +1,15 @@
+//
+//  SeatInfo.swift
+//  RealifeTech
+//
+//  Created by Mickey Lee on 23/06/2021.
+//  Copyright © 2021 Realife Tech. All rights reserved.
+//
+
+import Foundation
+
+public struct SeatInfo {
+
+    public let key: String
+    public let value: String?
+}
