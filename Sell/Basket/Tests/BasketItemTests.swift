cf605c8f8ac02ba0708a34c1c570d976d1f8b2e6
Mickey Lee <mickey.lee@realifetech.com>
2021-08-11 15:47:05 +0100

    WOD-22 Unit test Basket models

---
+++ Sell/Basket/Tests/BasketItemTests.swift
@@ -0,0 +1,40 @@
+//
+//  BasketItemTests.swift
+//  RealifeTechTests
+//
+//  Created by Mickey Lee on 11/08/2021.
+//  Copyright © 2021 Realife Tech. All rights reserved.
+//
+
+import XCTest
+import GraphQL
+@testable import RealifeTech
+
+final class BasketItemTests: XCTestCase {
+
+    func test_initWithApolloFragment_withResponse_validBasketItem() throws {
+        let item =  ApolloType.FragmentBasket.Item(
+            id: "1",
+            price: 100,
+            modifierItemsPrice: 0,
+            quantity: 1,
+            totalPrice: 100,
+            fulfilmentPoint: nil,
+            productVariant: nil,
+            product: nil,
+            productModifierItems: [
+                ApolloType.FragmentBasket.Item.ProductModifierItem(unsafeResultMap: [:])
+            ])
+        let sut = try XCTUnwrap(BasketItem(response: item))
+        XCTAssertEqual(sut.id, item.id)
+        XCTAssertEqual(sut.price, item.price)
+        XCTAssertEqual(sut.modifierItemsPrice, item.modifierItemsPrice)
+        XCTAssertEqual(sut.quantity, item.quantity)
+        XCTAssertEqual(sut.totalPrice, item.totalPrice)
+        XCTAssertEqual(sut.productModifierItems?.count, item.productModifierItems?.count)
+    }
+
+    func test_initWillApolloFragment_withNilResponse_returnNil() {
+        XCTAssertNil(BasketItem(response: nil))
+    }
+}
---
+++ Sell/Basket/Tests/ProductModifierItemSelectionTests.swift
@@ -0,0 +1,38 @@
+//
+//  ProductModifierItemSelectionTests.swift
+//  RealifeTechTests
+//
+//  Created by Mickey Lee on 11/08/2021.
+//  Copyright © 2021 Realife Tech. All rights reserved.
+//
+
+import XCTest
+import GraphQL
+@testable import RealifeTech
+
+final class ProductModifierItemSelectionTests: XCTestCase {
+
+    func test_publicInitialiser() {
+        let sut = ProductModifierItemSelection(
+            productModifierItem: ProductModifierItem(
+                id: "1",
+                additionalPrice: 10,
+                translations: [StandardTitleTranslation(language: "en", title: "A")]),
+            quantity: 1,
+            totalPrice: 10)
+        XCTAssertEqual(sut.productModifierItem?.id, "1")
+        XCTAssertEqual(sut.productModifierItem?.additionalPrice, 10)
+        XCTAssertEqual(sut.quantity, 1)
+        XCTAssertEqual(sut.totalPrice, 10)
+    }
+
+    func test_initWithFragment() {
+        let fragment = ApolloType.FragmentProductModifierItemSelection(
+            productModifierItem: nil,
+            quantity: 1,
+            totalPrice: 100)
+        let sut = ProductModifierItemSelection(response: fragment)
+        XCTAssertEqual(sut.quantity, fragment.quantity)
+        XCTAssertEqual(sut.totalPrice, fragment.totalPrice)
+    }
+}
