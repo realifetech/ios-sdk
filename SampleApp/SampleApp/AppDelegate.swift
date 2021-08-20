60d4a17b1428b1ce182b6ffaea8a287b946e0a56
Mickey Lee <mickey.lee@realifetech.com>
2021-08-20 15:11:02 +0100

    Rename DummyProject to SampleApp

--- SampleApp/SampleApp/AppDelegate.swift
+++ SampleApp/SampleApp/AppDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.swift
-//  DummyProject
+//  SampleApp
 //
 //  Created by Realife Tech on 23/09/2020.
 //  Copyright © 2020 Realife Tech. All rights reserved.
@@ -12,8 +12,10 @@ import RealifeTech
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
         let configuration = SDKConfiguration(
             appCode: "",
             clientSecret: "",
@@ -61,9 +63,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication,
-                     configurationForConnecting connectingSceneSession: UISceneSession,
-                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
--- SampleApp/SampleApp/SceneDelegate.swift
+++ SampleApp/SampleApp/SceneDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  SceneDelegate.swift
-//  DummyProject
+//  SampleApp
 //
 //  Created by Realife Tech on 23/09/2020.
 //  Copyright © 2020 Realife Tech. All rights reserved.
